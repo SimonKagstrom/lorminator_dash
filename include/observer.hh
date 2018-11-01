25a447c4958e5ad883799ca735f6bff4729593c9
Simon Kagstrom <simon.kagstrom@gmail.com>
2018-11-01 21:11:02 +0100

    observer: Refactor to use more common code, avoid shared_ptrs

--- include/observer.hh
+++ include/observer.hh
@@ -34,10 +34,10 @@ protected:
     virtual void detach(uint32_t id) = 0;
 };
 
-// How I'd like it to work
 template<typename FN>
 class NotifierContainer : public NotifierBase
 {
+public:
     std::unique_ptr<ObserverCookie> listen(FN &cb)
     {
         auto id = (uint32_t)m_listeners.size();
@@ -47,7 +47,6 @@ class NotifierContainer : public NotifierBase
         return std::make_unique<ObserverCookie>(id, shared_from_this());
     }
 
-protected:
     void detach(uint32_t id) override
     {
         m_listeners.erase(id);
@@ -56,121 +55,70 @@ protected:
     std::unordered_map<uint32_t, FN> m_listeners;
 };
 
-class Notifier0 : public NotifierBase
+template<typename FN>
+class NotifierBasex
+{
+public:
+    std::unique_ptr<ObserverCookie> listen(FN cb)
+    {
+        return m_impl->listen(cb);
+    }
+
+protected:
+    NotifierBasex() : m_impl(std::make_shared<NotifierContainer<FN>>())
+    {
+    }
+
+    std::shared_ptr<NotifierContainer<FN>> m_impl;
+};
+
+class Notifier0 : public NotifierBasex<std::function<void()>>
 {
 public:
     void invoke()
     {
-        for (auto &kv : m_listeners)
+        for (auto &kv : m_impl->m_listeners)
         {
             kv.second();
         }
     }
-
-    std::unique_ptr<ObserverCookie> listen(std::function<void()> cb)
-    {
-        auto id = (uint32_t)m_listeners.size();
-
-        m_listeners[id] = cb;
-
-        return std::make_unique<ObserverCookie>(id, shared_from_this());
-    }
-
-protected:
-    void detach(uint32_t id) override
-    {
-        m_listeners.erase(id);
-    }
-
-    std::unordered_map<uint32_t, std::function<void()>> m_listeners;
 };
 
 template<typename A0>
-class Notifier1 : public NotifierBase
+class Notifier1 : public NotifierBasex<std::function<void(A0)>>
 {
 public:
     void invoke(A0 arg0)
     {
-        for (auto &kv : m_listeners)
+        for (auto &kv : NotifierBasex<std::function<void(A0)>>::m_impl->m_listeners)
         {
             kv.second(arg0);
         }
     }
-
-    std::unique_ptr<ObserverCookie> listen(std::function<void(A0)> cb)
-    {
-        auto id = (uint32_t)m_listeners.size();
-
-        m_listeners[id] = cb;
-
-        return std::make_unique<ObserverCookie>(id, shared_from_this());
-    }
-
-protected:
-    void detach(uint32_t id) override
-    {
-        m_listeners.erase(id);
-    }
-
-    std::unordered_map<uint32_t, std::function<void(A0)>> m_listeners;
 };
 
 template<typename A0, typename A1>
-class Notifier2 : public NotifierBase
+class Notifier2 : public NotifierBasex<std::function<void(A0, A1)>>
 {
 public:
     void invoke(A0 arg0, A1 arg1)
     {
-        for (auto &kv : m_listeners)
+        for (auto &kv : NotifierBasex<std::function<void(A0, A1)>>::m_impl->m_listeners)
         {
             kv.second(arg0, arg1);
         }
     }
-
-    std::unique_ptr<ObserverCookie> listen(std::function<void(A0, A1)> cb)
-    {
-        auto id = (uint32_t)m_listeners.size();
-
-        m_listeners[id] = cb;
-
-        return std::make_unique<ObserverCookie>(id, shared_from_this());
-    }
-
-protected:
-    void detach(uint32_t id) override
-    {
-        m_listeners.erase(id);
-    }
-
-    std::unordered_map<uint32_t, std::function<void(A0, A1)>> m_listeners;
 };
 
 template<typename A0, typename A1, typename A2>
-class Notifier3 : public NotifierBase
+class Notifier3 : public NotifierBasex<std::function<void(A0, A1, A2)>>
 {
 public:
     void invoke(A0 arg0, A1 arg1, A2 arg2)
     {
-        for (auto &kv : m_listeners)
+        for (auto &kv : NotifierBasex<std::function<void(A0, A1, A2)>>::m_impl->m_listeners)
         {
             kv.second(arg0, arg1, arg2);
         }
     }
-
-    std::unique_ptr<ObserverCookie> listen(std::function<void(A0, A1, A2)> cb)
-    {
-        auto id = (uint32_t)m_listeners.size();
-
-        m_listeners[id] = cb;
-
-        return std::make_unique<ObserverCookie>(id, shared_from_this());
-    }
-
-protected:
-    void detach(uint32_t id) override
-    {
-        m_listeners.erase(id);
-    }
-
-    std::unordered_map<uint32_t, std::function<void(A0, A1, A2)>> m_listeners;
 };
