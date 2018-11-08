a716414ac9a2cbeb37685468e960f40a01485354
Simon Kagstrom <simon.kagstrom@gmail.com>
2018-11-08 18:24:31 +0100

    game: Init behavior for the current level

--- src/game.cc
+++ src/game.cc
@@ -2,6 +2,7 @@
 #include <level.hh>
 #include <entity.hh>
 #include <entity-properties.hh>
+#include <behavior.hh>
 
 #include <memory>
 
@@ -35,14 +36,26 @@ private:
     class CurrentLevel
     {
     public:
-        CurrentLevel(std::shared_ptr<ILevel> level)
+        CurrentLevel(std::shared_ptr<ILevel> level) :
+            m_level(level),
+            m_entityStore(IEntityStore::getInstance()),
+            m_entityProperties(IEntityProperties::getInstance())
         {
+            auto entities = m_entityStore->getEntities();
+
+            // Create behavior
+            for (auto &it : entities)
+            {
+                m_behavior[it->getId()] = IBehavior::fromEntity(m_level, it);
+            }
         }
 
     private:
         std::shared_ptr<ILevel> m_level;
         std::shared_ptr<IEntityStore> m_entityStore;
         std::shared_ptr<IEntityProperties> m_entityProperties;
+
+        std::unordered_map<uint32_t, std::unique_ptr<IBehavior>> m_behavior;
     };
 
     std::unique_ptr<CurrentLevel> m_currentLevel;
