f08c2069364c41243e676f3b4321144497b7c18c
Simon Kagstrom <simon.kagstrom@gmail.com>
2018-11-09 14:58:07 +0100

    point: tabs -> spaces

--- include/point.hh
+++ include/point.hh
@@ -5,98 +5,98 @@
 
 enum class Direction
 {
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT,
-	NONE,   // No direction at all
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    NONE,   // No direction at all
 };
 
 struct point
 {
-	int x{0};
-	int y{0};
+    int x{0};
+    int y{0};
 
-	point operator+(const Direction other) const
-	{
-		auto out = *this;
+    point operator+(const Direction other) const
+    {
+        auto out = *this;
 
-		if (other == Direction::UP)
-		{
-			out.y--;
-		}
-		else if (other == Direction::DOWN)
-		{
-			out.y++;
-		}
-		else if (other == Direction::LEFT)
-		{
-			out.x--;
-		}
-		else if (other == Direction::RIGHT)
-		{
-			out.x++;
-		}
+        if (other == Direction::UP)
+        {
+            out.y--;
+        }
+        else if (other == Direction::DOWN)
+        {
+            out.y++;
+        }
+        else if (other == Direction::LEFT)
+        {
+            out.x--;
+        }
+        else if (other == Direction::RIGHT)
+        {
+            out.x++;
+        }
 
-		return out;
-	}
+        return out;
+    }
 
-	point operator+(const point &other) const
-	{
-		return {x + other.x, y + other.y};
-	}
+    point operator+(const point &other) const
+    {
+        return {x + other.x, y + other.y};
+    }
 
-	point operator-(const point &other) const
-	{
-		return {x - other.x, y - other.y};
-	}
+    point operator-(const point &other) const
+    {
+        return {x - other.x, y - other.y};
+    }
 
-	bool operator==(const point &other) const
-	{
-		return x == other.x && y == other.y;
-	}
+    bool operator==(const point &other) const
+    {
+        return x == other.x && y == other.y;
+    }
 
-	bool operator!=(const point &other) const
-	{
-		return !(*this == other);
-	}
+    bool operator!=(const point &other) const
+    {
+        return !(*this == other);
+    }
 
-	bool operator<(const point &other) const
-	{
-		if (y < other.y)
-		{
-			return true;
-		}
+    bool operator<(const point &other) const
+    {
+        if (y < other.y)
+        {
+            return true;
+        }
 
-		if (y > other.y)
-		{
-			return false;
-		}
+        if (y > other.y)
+        {
+            return false;
+        }
 
-		return x < other.x;
-	}
+        return x < other.x;
+    }
 
-	point operator*(int val) const
-	{
-		return {x * val, y * val};
-	}
+    point operator*(int val) const
+    {
+        return {x * val, y * val};
+    }
 };
 
 struct extents
 {
-	unsigned width{0};
-	unsigned height{0};
+    unsigned width{0};
+    unsigned height{0};
 
-	bool operator==(const extents &other) const
-	{
-		return width == other.width &&
-				height == other.height;
-	}
+    bool operator==(const extents &other) const
+    {
+        return width == other.width &&
+                height == other.height;
+    }
 
-	bool operator!=(const extents &other) const
-	{
-		return !(*this == other);
-	}
+    bool operator!=(const extents &other) const
+    {
+        return !(*this == other);
+    }
 };
 
 static std::ostream& operator << (std::ostream& os, point const& value)
