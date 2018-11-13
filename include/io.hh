dd315902f5a4a21a2487fa7a4a339544561d3a9b
Simon Kagstrom <simon.kagstrom@gmail.com>
2018-11-13 20:42:58 +0100

    io: unit test: Correct mock, remove old display method

--- include/io.hh
+++ include/io.hh
@@ -19,7 +19,6 @@ public:
 
     virtual void setup(uint32_t windowWidth, uint32_t windowHeight) = 0;
 
-    virtual void display(const point &center, std::shared_ptr<ILevel> level, std::shared_ptr<IEntityStore> store) = 0;
     virtual void display(const std::shared_ptr<IEntity> center, std::shared_ptr<ILevel> level, const std::unordered_map<uint32_t, std::shared_ptr<IAnimator>> &animators) = 0;
     virtual uint32_t msSince(uint32_t last) = 0;
     virtual void delay(uint32_t ms) = 0;
