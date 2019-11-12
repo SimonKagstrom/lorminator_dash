f05c6aec3ebf8b190ec3495be447c4b21795c5d5
Simon Kagstrom <simon.kagstrom@gmail.com>
2019-11-12 17:22:49 +0100

    traits: colliding: Refactor collisions

--- src/traits/colliding.cc
+++ src/traits/colliding.cc
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <vector>
+#include <functional>
 
 class CollisionTrait : public ITrait
 {
@@ -22,18 +23,41 @@ public:
 
     void onCollision(std::shared_ptr<IEntity> one, std::shared_ptr<IEntity> other)
     {
-        if (one->getType() == EntityType::FIREBALL)
+        ifOne(one, other, EntityType::FIREBALL,
+            [](std::shared_ptr<IEntity> which)
         {
-            other->remove();
-        }
-        else if (other->getType() == EntityType::FIREBALL)
-        {
-            one->remove();
-        }
+            which->remove();
+        });
 
         // More to come
     }
 
 private:
+    void ifOne(std::shared_ptr<IEntity> one, std::shared_ptr<IEntity> other, EntityType type,
+        std::function<void(std::shared_ptr<IEntity>)> onTrue) const
+    {
+        if (one->getType() == type)
+        {
+            onTrue(other);
+        }
+        else if (other->getType() == type)
+        {
+            onTrue(one);
+        }
+    }
+
+    void ifPair(std::shared_ptr<IEntity> one, std::shared_ptr<IEntity> other, EntityType type, EntityType type2,
+        std::function<void(std::shared_ptr<IEntity>)> onTrue) const
+    {
+        if (one->getType() == type && other->getType() == type2)
+        {
+            onTrue(other);
+        }
+        else if (other->getType() == type && one->getType() == type2)
+        {
+            onTrue(one);
+        }
+    }
+
     std::unique_ptr<ObserverCookie> m_cookie;
 };
